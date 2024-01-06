fa768d88aed7c4ad010140e87f61c787bce4393f
João Pedro <joaoplay16@gmail.com>
2024-01-06 19:12:30 -0300

    Adiciona endpoints para manipulação de usuarios

--- src/server/routes/usuario.ts
+++ src/server/routes/usuario.ts
@@ -1,9 +1,16 @@
 import { type Router, type Request, type Response } from "express"
+import { UsuarioController } from "../../controllers/index.mjs"
+import { PontoController } from "../../controllers/index.mjs"
 
 module.exports = function (router: Router) {
   router.get("/", function (req: Request, res: Response) {
     res.send("Collaborator")
   })
+
+  router.get("/usuario/:id", UsuarioController.details)
+  router.post("/usuario/criar", UsuarioController.save)
+
+  router.get("/usuario/:id_usuario/pontos", PontoController.index)
 
   return router
 }
