1d50013f1f6d1b79162ebe5e024f85032735a4fc
João Pedro <joaoplay16@gmail.com>
2024-01-06 19:00:12 -0300

    Cria tipos Ponto e Usuario

---
+++ src/types/ponto.d.ts
@@ -0,0 +1,8 @@
+type Ponto = {
+    id: number,
+    data: string,
+    hora_entrada: string,
+    hora_saida: string
+}
+
+export default Ponto
---
+++ src/types/usuario.d.ts
@@ -0,0 +1,13 @@
+
+type Usuario = {
+    id: number,
+    nome: string,
+    nome_de_usuario: string,
+    email: string,
+    senha: string,
+    celular: string,
+    e_admin: number,
+    pontos: Ponto[]
+}
+
+export default Usuario
