028bbb3f6b91c9c90457672e5cc1d8182b9479c4
João Pedro <joaoplay16@gmail.com>
2024-01-07 19:35:43 -0300

    Adiciona endpoint busca de todos os usuarios

--- src/server/routes/admin.ts
+++ src/server/routes/admin.ts
@@ -1,9 +1,12 @@
 import { type Router, type Request, type Response } from "express"
+import { UsuarioController } from "../../controllers/index.mjs"
 
 module.exports = function (router: Router) {
   router.get("/admin/", function (req: Request, res: Response) {
     res.send("Admin")
   })
+
+  router.get("/admin/usuarios", UsuarioController.index)
 
   return router
 }
