5cfbf4ef06df6c21374224310be558eb3cddbe20
João Pedro <joaoplay16@gmail.com>
2024-01-07 19:37:37 -0300

    Adciona endpoint para o relatorio de horas trabalhadas de todos os usuarios

--- src/server/routes/admin.ts
+++ src/server/routes/admin.ts
@@ -1,5 +1,6 @@
 import { type Router, type Request, type Response } from "express"
 import { UsuarioController } from "../../controllers/index.mjs"
+import { PontoController } from "../../controllers/index.mjs"
 
 module.exports = function (router: Router) {
   router.get("/admin/", function (req: Request, res: Response) {
@@ -7,6 +8,8 @@ module.exports = function (router: Router) {
   })
 
   router.get("/admin/usuarios", UsuarioController.index)
+
+  router.get("/admin/ponto/relatorio", PontoController.usersReport)
 
   return router
 }
