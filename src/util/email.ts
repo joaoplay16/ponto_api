5722cacbaf7dadab1808a7d8fbb67552e2468f4e
João Pedro <joaoplay16@gmail.com>
2024-01-09 19:24:07 -0300

    Adicionad funcao geradora de template para redefinição de senha

--- src/util/email.ts
+++ src/util/email.ts
@@ -66,6 +66,77 @@ export function generateUserRegisterTemplate(
     `
 }
 
+export function generatePasswordRedefinitionTemplate(
+  nome: string,
+  registrationLink: string
+) {
+  return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Redefinição de senha</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+    }
+
+    .container {
+      max-width: 600px;
+      margin: 20px auto;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+      color: #333;
+    }
+
+    p {
+      color: #555;
+    }
+
+    .cta-button {
+      display: inline-block;
+      padding: 10px 20px;
+      margin-top: 20px;
+      background-color: #007bff;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+
+    .cta-button:hover {
+      background-color: #0056b3;
+    }
+
+    a {
+      display: block;
+      word-wrap: break-word; /* Allow long words to be broken and wrap to the next line */
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Redefinição de senha</h1>
+    <p>Olá, ${nome}</p>
+    <p> Para redefinir a sua senha, clique no botão abaixo:</p>
+    <a href="${registrationLink}" class="cta-button">Redefinir senha</a>
+    <p>Se o botão acima não funcionar, você também pode copiar e colar o seguinte link em seu navegador:</p>
+    <a href="${registrationLink}">${registrationLink}</a>
+    <p>Obrigado!</p>
+  </div>
+</body>
+</html>
+`
+}
+
 export function isEmail(email: string) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
