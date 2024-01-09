0f34628ecaf6536f71a66e65fa6160a230179a4d
João Pedro <joaoplay16@gmail.com>
2024-01-09 14:45:56 -0300

    Protege as rotas de usuario e administrador com autenticacao

--- src/server/routes/admin.ts
+++ src/server/routes/admin.ts
@@ -1,10 +1,15 @@
 import { type Router, type Request, type Response } from "express"
 import { UsuarioController } from "../../controllers/index.mjs"
 import { PontoController } from "../../controllers/index.mjs"
+import { isAdminAuthorized } from "../authMiddleware"
 
 module.exports = function (router: Router) {
-  router.get("/admin/usuarios", UsuarioController.index)
-  router.get("/admin/ponto/relatorio", PontoController.usersReport)
+  router.get("/admin/usuarios", isAdminAuthorized, UsuarioController.index)
+  router.get(
+    "/admin/ponto/relatorio",
+    isAdminAuthorized,
+    PontoController.usersReport
+  )
 
   return router
 }
--- src/server/routes/usuario.ts
+++ src/server/routes/usuario.ts
@@ -1,15 +1,28 @@
 import { type Router, type Request, type Response } from "express"
 import { UsuarioController } from "../../controllers/index.mjs"
 import { PontoController } from "../../controllers/index.mjs"
+import {isUserAuthorized} from "../authMiddleware"
 
 module.exports = function (router: Router) {
   router.post("/usuario/registro", UsuarioController.register)
-  router.get("/usuario/:id", UsuarioController.details)
+  router.get("/usuario/:id", isUserAuthorized, UsuarioController.details)
   router.post("/usuario/criar", UsuarioController.save)
-  router.post("/usuario/:id_usuario/atualizar", UsuarioController.update)
+  router.post(
+    "/usuario/:id_usuario/atualizar",
+    isUserAuthorized,
+    UsuarioController.update
+  )
 
-  router.get("/usuario/:id_usuario/pontos", PontoController.index)
-  router.get("/usuario/:id_usuario/relatorio", PontoController.userReport)
+  router.get(
+    "/usuario/:id_usuario/pontos",
+    isUserAuthorized,
+    PontoController.index
+  )
+  router.get(
+    "/usuario/:id_usuario/relatorio",
+    isUserAuthorized,
+    PontoController.userReport
+  )
 
   return router
 }
