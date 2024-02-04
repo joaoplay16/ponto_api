016c5ff52b552afa6629487718e7bf1dd5f8c8a2
João Pedro <joaoplay16@gmail.com>
2024-02-03 22:41:44 -0300

    Instala e configura dependendencia 'chai-as-promised'

--- src/tests/testhelper.ts
+++ src/tests/testhelper.ts
@@ -1,12 +1,14 @@
 import * as sinon from "sinon"
 import * as chai from "chai"
 import  sinonChai from "sinon-chai"
+import  chaiAsPromised from "chai-as-promised"
 
 declare global {
   var expect: Chai.ExpectStatic
 }
 
 chai.use(sinonChai)
+chai.use(chaiAsPromised)
 
 global.expect = chai.expect
 global.sinon = sinon
