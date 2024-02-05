dabac262788b5ad92778cc49e25cc43920813480
João Pedro <joaoplay16@gmail.com>
2024-02-05 20:04:48 -0300

    Corrige erro 'Cannot access [Model] before initialization'
    Este erro ocorre no codigo compilado.
    Usamos  ReturnType para representar o tipo da propiedade `usuario`

--- src/models/Ponto.ts
+++ src/models/Ponto.ts
@@ -11,7 +11,7 @@ import UsuarioModel from "./Usuario"
 
 @Table({ tableName: "pontos", timestamps: false })
 class PontoModel extends Model {
-   @Column({ type: DataTypes.STRING })({
+   @Column({
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
@@ -29,7 +29,7 @@ class PontoModel extends Model {
   declare usuario_id: number
 
   @BelongsTo(() => UsuarioModel)
-  declare usuario: UsuarioModel
+  declare usuario: ReturnType<() => UsuarioModel>
 }
 
 export default PontoModel
