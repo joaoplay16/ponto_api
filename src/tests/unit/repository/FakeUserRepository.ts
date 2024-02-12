14f9885bbd7079713850a222977b1aca1a6c9997
João Pedro <joaoplay16@gmail.com>
2024-02-12 20:05:32 -0300

    Implementa busca fake no metodo getUsers

--- src/tests/unit/repository/FakeUserRepository.ts
+++ src/tests/unit/repository/FakeUserRepository.ts
@@ -1,7 +1,31 @@
-import Usuario from "../../../types/usuario"
+import Usuario, { UsersQueryResult } from "../../../types/usuario"
 import UserRepository from "../../../repository/UserRepository"
 
 class FakeUserRepository implements UserRepository {
+  users: UsersQueryResult = { rows: [], count: 0 }
+
+  getUsers(
+    cargo: string,
+    limit: number,
+    offset: number
+  ): Promise<UsersQueryResult> {
+    let filteredUsers = this.users.rows
+
+    if (cargo) {
+      filteredUsers = filteredUsers.filter((user) => user.cargo === cargo)
+    }
+
+    if (limit && offset) {
+      const startIndex = offset
+      const endIndex = startIndex + limit
+      filteredUsers = filteredUsers.slice(startIndex, endIndex)
+    }
+
+    this.users.rows = filteredUsers
+    this.users.count = filteredUsers.length
+
+    return Promise.resolve(this.users)
+  }
   async findUserByEmail(email: string): Promise<Usuario | null> {
     return {
       id: 1,
@@ -14,7 +38,7 @@ class FakeUserRepository implements UserRepository {
       criado_em: "2024-01-01",
       e_admin: 0,
       ativo: 1,
-      pontos: []
+      pontos: [],
     }
   }
 }
