0032607145c0827372c864cb73a0b8a7545a5660
João Pedro <joaoplay16@gmail.com>
2024-02-11 23:15:40 -0300

    Adiciona metodo getUsers à interface UserRepository

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,7 +1,12 @@
-import Usuario from "../types/usuario"
+import Usuario, { UsersQueryResult } from "../types/usuario"
 
 interface UserRepository {
   findUserByEmail(email: string): Promise<Usuario | null>
+  getUsers(
+    cargo: string,
+    limit: number,
+    offset: number
+  ): Promise<UsersQueryResult>
 }
 
 export default UserRepository
