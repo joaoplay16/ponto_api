794b5389fe9c77b2e186216e64178f0bf86b514c
João Pedro <joaoplay16@gmail.com>
2024-02-11 23:16:16 -0300

    Implementa metodo getUsers na classe DefaultUserRepository

--- src/repository/DefaultUserRepository.ts
+++ src/repository/DefaultUserRepository.ts
@@ -1,8 +1,22 @@
+import DatabaseOperationError from "../errors/DatabaseOperationError"
 import UsuarioModel from "../models/Usuario"
-import Usuario from "../types/usuario"
+import Usuario, { UsersQueryResult } from "../types/usuario"
 import UserRepository from "./UserRepository"
 
 class DefaultUserRepository implements UserRepository {
+  async getUsers(
+    cargo: string,
+    limit: number,
+    offset: number
+  ): Promise<UsersQueryResult> {
+    return await UsuarioModel.findAndCountAll({
+      where: { cargo },
+      limit: limit,
+      offset: offset,
+      order: [["nome", "ASC"]],
+      raw: true,
+    })
+  }
   async findUserByEmail(email: string): Promise<Usuario | null> {
     return await UsuarioModel.findOne({
       where: { email: email },
