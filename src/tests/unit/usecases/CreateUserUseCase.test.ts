3351a33ad34590abbde5604e28f09c71d85d93a6
João Pedro <joaoplay16@gmail.com>
2024-02-15 20:56:56 -0300

    Testa CreateUserUseCase

---
+++ src/tests/unit/usecases/CreateUserUseCase.test.ts
@@ -0,0 +1,81 @@
+import ApiRequestError from "../../../errors/ApiRequestError"
+import Usuario from "../../../types/usuario"
+import CreateUserUseCase from "../../../usecases/CreateUserUseCase"
+import FakeUserRepository from "../repository/FakeUserRepository"
+
+describe("Test user creation", () => {
+  let fakeUserRepository: FakeUserRepository
+  let createUserUseCase: CreateUserUseCase
+
+  beforeEach(() => {
+    fakeUserRepository = new FakeUserRepository()
+    createUserUseCase = new CreateUserUseCase(fakeUserRepository)
+  })
+
+  it("Should throw ApiRequestError when receive empty parameters", async () => {
+    let userToSave: Usuario = {
+      id: 1,
+      nome: "", // Ivalid: empty
+      cargo: "colaborador",
+      nome_de_usuario: "savazzo",
+      email: "savazzo@example.com",
+      senha: "senha123",
+      celular: "12345678901",
+      criado_em: "2024-01-01",
+      e_admin: 0,
+      ativo: 1,
+      pontos: [],
+    }
+
+    await expect(
+      createUserUseCase.save(userToSave)
+    ).to.be.eventually.rejected.and.deep.equal(
+      new ApiRequestError(
+        400,
+        "Requisição inválida. Verifique os parâmetros fornecidos."
+      )
+    )
+  })
+
+  it("Should throw ApiRequestError when receive an invalid e-mail", async () => {
+    let userToSave: Usuario = {
+      id: 1,
+      nome: "Savatori Piazzo",
+      cargo: "colaborador",
+      nome_de_usuario: "savazzo",
+      email: "savazzo#example.com",
+      senha: "senha123",
+      celular: "12345678901",
+      criado_em: "2024-01-01",
+      e_admin: 0,
+      ativo: 1,
+      pontos: [],
+    }
+
+    await expect(
+      createUserUseCase.save(userToSave)
+    ).to.be.eventually.rejected.and.deep.equal(
+      new ApiRequestError(400, "Informe um e-mail válido.")
+    )
+  })
+
+  it("Should return the created user", async () => {
+    let userToSave: Usuario = {
+      id: 1,
+      nome: "Savatori Piazzo",
+      cargo: "colaborador",
+      nome_de_usuario: "savazzo",
+      email: "savazzo@example.com",
+      senha: "senha123",
+      celular: "12345678901",
+      criado_em: "2024-01-01",
+      e_admin: 0,
+      ativo: 1,
+      pontos: [],
+    }
+
+    await expect(
+      createUserUseCase.save(userToSave)
+    ).to.be.eventually.fulfilled.and.deep.equal(userToSave)
+  })
+})
