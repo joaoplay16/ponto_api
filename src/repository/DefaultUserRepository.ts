aa6cf3936254548ad8261e1837f7b1a414eb1cdd
João Pedro <joaoplay16@gmail.com>
2024-02-16 21:25:33 -0300

    Intercepta erros de duplicidade de usuario e relança para o chamador

--- src/repository/DefaultUserRepository.ts
+++ src/repository/DefaultUserRepository.ts
@@ -1,10 +1,29 @@
+import DatabaseOperationError from "../errors/DatabaseOperationError"
 import UsuarioModel from "../models/Usuario"
 import Usuario, { UsersQueryResult } from "../types/usuario"
 import UserRepository from "./UserRepository"
 
 class DefaultUserRepository implements UserRepository {
   async create(user: Usuario): Promise<Usuario> {
-    return await UsuarioModel.create(user)
+    try {
+      return await UsuarioModel.create(user)
+    } catch (error: any) {
+      if (error.original.code == "ER_DUP_ENTRY") {
+        switch (error.errors[0].path) {
+          case "nome_de_usuario":
+            throw new DatabaseOperationError(
+              409,
+              "Este nome de usuário já foi cadastrado"
+            )
+          case "email":
+            throw new DatabaseOperationError(
+              409,
+              "O endereço de e-mail já foi cadastrado"
+            )
+        }
+      }
+      throw error
+    }
   }
 
   async findUserById(id: number): Promise<Usuario | null> {
