0b9e33d7821b9d4d6032b8f072cfdbe8adf61ff7
João Pedro <joaoplay16@gmail.com>
2024-02-17 22:27:16 -0300

    Adiciona metodos `findUserByUsername` e `update` à interface UserRepository

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -8,7 +8,9 @@ interface UserRepository {
     offset: number
   ): Promise<UsersQueryResult>
   findUserById(id: number): Promise<Usuario | null>
+  findUserByUsername(username: string): Promise<Usuario | null>
   create(user: Usuario): Promise<Usuario | null>
+  update(user: Usuario): Promise<void>
 }
 
 export default UserRepository
