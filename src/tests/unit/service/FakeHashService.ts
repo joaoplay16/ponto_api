984239529526ce949ce11900b6830d33d287d33c
João Pedro <joaoplay16@gmail.com>
2024-02-19 20:44:49 -0300

    Implementa metodo `hashSync` em FakeHashService

--- src/tests/unit/service/FakeHashService.ts
+++ src/tests/unit/service/FakeHashService.ts
@@ -4,6 +4,9 @@ class FakeHashService implements HashService {
   compareSync(plaintext: string, hashed: string): boolean {
     return plaintext === hashed
   }
+  hashSync(plaintext: string): string {
+    return plaintext.split("").reverse().toString()
+  }
 }
 
 export default FakeHashService
