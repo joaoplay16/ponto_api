423fdbc6edaed68a3bfb98b031820bf46d79e577
João Pedro <joaoplay16@gmail.com>
2024-02-23 21:04:29 -0300

    Refatora `UsuarioController`
    Move logica de negocio para usecases

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,30 +1,41 @@
-import bcrypt from "bcrypt"
 import "dotenv/config"
 import { type Request, type Response } from "express"
-import jwt, { JwtPayload, Secret, VerifyErrors } from "jsonwebtoken"
-import UsuarioModel from "../models/Usuario"
-import UsuarioType from "../types/usuario"
-import sendMail, { EmailFields } from "../service/emailService"
-import { generatePasswordRedefinitionTemplate, generateUserRegisterTemplate, isEmail } from "../util/email"
+import ApiRequestError from "../errors/ApiRequestError"
+import DatabaseOperationError from "../errors/DatabaseOperationError"
+import DefaultUserRepository from "../repository/DefaultUserRepository"
+import BcryptHashService from "../service/BcryptHashService"
+import GmailService from "../service/GmailService"
+import JwtTokenService from "../service/JwtTokenService"
+import { default as Usuario, default as UsuarioType } from "../types/usuario"
+import CreateUserUseCase from "../usecases/CreateUserUseCase"
+import FinishUserRegisterUseCase from "../usecases/FinishUserRegisterUseCase"
+import GetUserDetailsUseCase from "../usecases/GetUserDetailsUseCase"
+import GetUsersUseCase from "../usecases/GetUsersUseCase"
+import PasswordResetUsecase from "../usecases/PasswordResetUsecase"
+import RequestPasswordResetUsecase from "../usecases/RequestPasswordResetUsecase"
+import UpdateUserUseCase from "../usecases/UpdateUserUseCase"
+import { generateUserRegisterTemplate } from "../util/email"
 
 const UsuarioController = {
   async index(req: Request, res: Response): Promise<void> {
     try {
-      const { limit, offset, cargo} = req.query
+      const { limit, offset, cargo } = req.query
 
-      const usuarios = await UsuarioModel.findAndCountAll({
-        where: {cargo: cargo as string},
-        limit: parseInt(limit as string) || 20,
-        offset: parseInt(offset as string) || 0,
-        order: [["nome", "ASC"]],
-      })
-      if (usuarios != null) {
-        res.json(usuarios)
-      } else {
-        res.status(404).json({ error: "Nenhum usuário encontrado" })
-      }
-    } catch (error) {
+      const usecase = new GetUsersUseCase(new DefaultUserRepository())
+
+      const usuarios = await usecase.find(
+        cargo as string,
+        limit as string,
+        offset as string
+      )
+
+      res.json(usuarios)
+    } catch (error: unknown) {
       console.error("Erro ao buscar usuários:", error)
+      if (error instanceof DatabaseOperationError) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
+        return
+      }
       res.status(500).json({ error: "Erro interno do servidor" })
     }
   },
@@ -33,123 +44,82 @@ const UsuarioController = {
     try {
       const { id } = req.params
 
-      var result = await UsuarioModel.findByPk(id)
+      const getUserDetailsUseCase = new GetUserDetailsUseCase(
+        new DefaultUserRepository()
+      )
+
+      var result = await getUserDetailsUseCase.findUserById(id)
       if (result != null) {
-        const { senha, ...usuarioSemASenha }: UsuarioModel = result.dataValues;
+        const { senha, ...usuarioSemASenha }: Usuario = result
 
         res.json(usuarioSemASenha)
       } else {
         res.status(404).json({ error: "Usuário não encontrado" })
       }
     } catch (error) {
-      console.error("Erro ao buscar usuário:", error)
-      res.status(404).json({ error: "Usuário não encontrado" })
+      res.status(500).json({ error: "Erro interno do servidor" })
     }
   },
 
   async save(req: Request, res: Response): Promise<void> {
     try {
-      const usuarioInfo: UsuarioModel = req.body
+      const usuarioInfo: UsuarioType = req.body
 
-      const { nome, email, nome_de_usuario, celular } = usuarioInfo
-
-      if (
-        ![nome, email, nome_de_usuario, celular].every(
-          (campo) => campo && campo.length > 0
-        )
-      ) {
-        res.status(400).json({
-          error: "Requisição inválida. Verifique os parâmetros fornecidos."
-        })
-        return
-      }
-
-      if(!isEmail(email)){
-        res.status(400).json({
-          error: "Informe um e-mail válido.",
-        })
-        return
-      }
+      const createUserUseCase = new CreateUserUseCase(
+        new DefaultUserRepository()
+      )
 
       const novoUsuario: UsuarioType = usuarioInfo
-      
-      const usuarioCriado = await UsuarioModel.create(novoUsuario)
 
-      if (usuarioCriado != null) {
-        res.json(usuarioCriado)
-        sendMail({
-          to: email,
-          subject: "EmPonto - Continue seu cadastro",
-          html: generateUserRegisterTemplate(
-            nome,
-            `${process.env.USER_REGISTRATION_URL}?usuario=${nome_de_usuario}`
-          )
-        } as EmailFields)
-      } else {
-        res
-          .status(500)
-          .json({ error: "Erro interno do servidor falha ao criar usuário" })
-      }
-    } catch (error: any) {
-      console.error("Erro interno do servidor", error)
-      if (error.original.code == "ER_DUP_ENTRY") {
-        switch (error.errors[0].path) {
-          case "nome_de_usuario":
-            res.status(409).json({ error: "Este nome usuário já foi cadastrado" })
-            break
-          case "email":
-            res.status(409).json({ error: "O endereço de e-mail já foi cadastrado" })
-            break
-        }
-       return
+      const usuarioCriado = await createUserUseCase.save(novoUsuario)
+
+      res.json(usuarioCriado)
+
+      new GmailService().sendMail({
+        to: novoUsuario.email,
+        subject: "EmPonto - Continue seu cadastro",
+        html: generateUserRegisterTemplate(
+          novoUsuario.nome,
+          `${process.env.USER_REGISTRATION_URL}?usuario=${novoUsuario.nome_de_usuario}`
+        ),
+      })
+    } catch (error: unknown) {
+      if (error instanceof ApiRequestError) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
+        return
       }
 
-      res.status(500).json({ error: "Erro interno do servidor", info: error })
+      if (error instanceof DatabaseOperationError) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
+        return
+      }
+
+      res.status(500).json({ error: "Erro interno do servidor" })
     }
   },
 
   // Define a senha do usuário pré-registrado
   async register(req: Request, res: Response): Promise<void> {
     try {
+      const { usuario: username, senha: password } = req.body
 
-      const { usuario: usuarioParam, senha } = req.body
-      
-      const usuario = await UsuarioModel.findOne({
-        where: {nome_de_usuario: usuarioParam}
-      })
+      const finishUserRegisterUsecase = new FinishUserRegisterUseCase(
+        new DefaultUserRepository(),
+        new BcryptHashService()
+      )
 
-      if(usuario?.senha) {
-        res.status(409).json({
-          error: "Este usuário já foi cadastrado.",
-        })
+      await finishUserRegisterUsecase.finishRegister(username, password)
+
+      res.send()
+    } catch (error: unknown) {
+      if (
+        error instanceof ApiRequestError ||
+        error instanceof DatabaseOperationError
+      ) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
         return
       }
 
-      var hashDaSenha = ""
-
-      if(senha){
-          if(senha.length >= 8){
-            hashDaSenha = bcrypt.hashSync(senha, 10)
-          }else{
-            res.status(400).json({
-              error: "A senha deve ter 8 ou mais caracteres.",
-            })
-            return
-          }
-      }
-
-      const resultado = await UsuarioModel.update(
-        {senha: hashDaSenha},
-        {
-          where: { id: usuario?.id },
-        }
-      )
-
-      res.json(resultado)
-      
-    } catch (error: any) {
-      console.error("Erro interno do servidor", error)
-      
       res.status(500).json({ error: "Erro interno do servidor", info: error })
     }
   },
@@ -158,185 +128,74 @@ const UsuarioController = {
     try {
       const { email } = req.query
 
-      if (!isEmail(email as string)) {
-        res.status(400).json({
-          error: "Informe um e-mail válido.",
-        })
+      const requestPasswordResetUsecase = new RequestPasswordResetUsecase(
+        new DefaultUserRepository(),
+        new GmailService()
+      )
+
+      await requestPasswordResetUsecase.sendRecoveryEmail(email as string)
+
+      res.send()
+    } catch (error: unknown) {
+      if (
+        error instanceof ApiRequestError ||
+        error instanceof DatabaseOperationError
+      ) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
         return
       }
-
-      const usuario = await UsuarioModel.findOne({
-        where: { email: email as string },
-      })
-
-      if (usuario == null) {
-        res.status(404).json({
-          error: "Este email não está cadastrado",
-        })
-        return
-      }
-
-      const jwtSecretKey = process.env.JWT_SECRET_KEY || ""
-      const token = jwt.sign({ email }, jwtSecretKey, { expiresIn: "20m" })
-
-      sendMail({
-        to: email,
-        subject: "EmPonto - Redefinição de senha",
-        html: generatePasswordRedefinitionTemplate(
-          usuario.nome,
-          `${process.env.PASSWORD_CHANGE_URL}?token=${token}`
-        ),
-      } as EmailFields)
-        .then(() => {
-          res.status(200).send()
-        })
-        .catch((e: any) => {
-          res
-            .status(500)
-            .json({ error: "Erro ao enviar e-mail para redefinição de senha" })
-        })
-    } catch (error: any) {
-      console.error("Erro interno do servidor", error)
 
       res.status(500).json({ error: "Erro interno do servidor", info: error })
     }
   },
-  
+
   async resetPassword(req: Request, res: Response): Promise<void> {
     try {
       const { token, senha } = req.body
 
-      const jwtSecretKey: Secret = process.env.JWT_SECRET_KEY || ""
-
-      jwt.verify(
-        token as string,
-        jwtSecretKey,
-        async (
-          err: VerifyErrors | null,
-          decoded: JwtPayload | string | undefined
-        ) => {
-          if (err) {
-            console.log("Erro de decodificação de token", err)
-            return res
-              .status(400)
-              .json({ error: "Token inválido ou expirado." })
-          }
-
-          if (decoded) {
-            const { email } = decoded as { email: string }
-
-            const count: number = await UsuarioModel.count({
-              where: { email },
-            })
-
-            // Verifica se usuario existe
-            if (count == 0) {
-              res.status(404).json({
-                error: "Este email não está cadastrado",
-              })
-              return
-            }
-
-            var hashDaSenha = ""
-
-            if (senha) {
-              if (senha.length >= 8) {
-                hashDaSenha = bcrypt.hashSync(senha, 10)
-              } else {
-                res.status(400).json({
-                  error: "A senha deve ter 8 ou mais caracteres.",
-                })
-                return
-              }
-            }
-
-            //Se correu tudo bem recebemos [1]
-            const linhasAfetadas: [number] = await UsuarioModel.update(
-              { senha: hashDaSenha },
-              {
-                where: { email },
-              }
-            )
-
-            return res.json(linhasAfetadas)
-          }
-        }
+      const passwordResetUsecase = new PasswordResetUsecase(
+        new DefaultUserRepository(),
+        new BcryptHashService(),
+        new JwtTokenService()
       )
-    } catch (error: any) {
-      console.error("Erro interno do servidor", error)
 
-      res.status(500).json({ error: "Erro interno do servidor", info: error })
+      await passwordResetUsecase.resetPassword(token, senha)
+      
+      res.send()
+    } catch (error: unknown) {
+      if (
+        error instanceof ApiRequestError ||
+        error instanceof DatabaseOperationError
+      ) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
+        return
+      }
+      res.status(500).json({ error: "Erro interno do servidor" })
     }
   },
 
   async update(req: Request, res: Response): Promise<void> {
     try {
       const { id_usuario } = req.params
-      const usuario: UsuarioModel = req.body
+      const usuario: Usuario = req.body
 
-      const { nome, email, cargo, nome_de_usuario, celular, senha } =
-        usuario
-
-      if (
-        ![nome, email, cargo, nome_de_usuario, celular].every(
-          (campo) => campo && campo.length > 0
-        )
-      ) {
-        res.status(400).json({
-          error: "Requisição inválida. Verifique os parâmetros fornecidos.",
-        })
-        return
-      }
-
-      var hashDaSenha = ""
-
-      if (senha) {
-        if (senha.length >= 8) {
-          hashDaSenha = bcrypt.hashSync(senha, 10)
-        } else {
-          res.status(400).json({
-            error: "A senha deve ter 8 ou mais caracteres.",
-          })
-          return
-        }
-      }
-     
-      const usuarioAtualizado = {
-        ...usuario,
-        ...(hashDaSenha && { senha: hashDaSenha }),
-      }
-
-      const resultado = await UsuarioModel.update(
-        usuarioAtualizado,
-        {
-          where: { id: id_usuario },
-        }
+      const updateUserUsecase = new UpdateUserUseCase(
+        new DefaultUserRepository(),
+        new BcryptHashService()
       )
 
-      if(resultado[0] < 0){
-        res.status(500).json({error: "Falha ao atualizar usuário"})
-        return
-      }
+      await updateUserUsecase.update({ ...usuario, id: Number(id_usuario) })
 
-      res.status(200).json(resultado)
-    } catch (error: any) {
-      console.error("Erro ao atualizar usuário:", error)
-      if (error.original.code == "ER_DUP_ENTRY") {
-        switch (error.errors[0].path) {
-          case "nome_de_usuario":
-            res
-              .status(500)
-              .json({ error: "Este nome usuário já foi cadastrado" })
-            break
-          case "email":
-            res
-              .status(500)
-              .json({ error: "O endereço de e-mail já foi cadastrado" })
-            break
-        }
+      res.send()
+    } catch (error: unknown) {
+      if (
+        error instanceof ApiRequestError ||
+        error instanceof DatabaseOperationError
+      ) {
+        res.status(error.statusCode).json({ error: error.errorMessage })
         return
       }
-      res.status(404).json({ error: "Erro interno do servidor" })
+      res.status(500).json({ error: "Erro interno do servidor" })
     }
   },
 }
