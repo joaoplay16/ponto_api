6f66043acf7734c88b66884b5f29a57fba47289f
João Pedro <joaoplay16@gmail.com>
2024-02-29 18:49:12 -0300

    Cria `WorkingHoursReportUsecase`

---
+++ src/usecases/WorkingHoursReportUsecase.ts
@@ -0,0 +1,49 @@
+import ApiRequestError from "../errors/ApiRequestError"
+import PontoRepository from "../repository/PontoRepository"
+import { PontosReportQueryResult } from "../types/ponto"
+
+class WorkingHoursReportUsecase {
+  private pontoRepository: PontoRepository
+
+  constructor(pontoRepository: PontoRepository) {
+    this.pontoRepository = pontoRepository
+  }
+
+  async workingHoursReport(
+    user_id?: string,
+    mes?: string,
+    ano?: string,
+    limit?: string,
+    offset?: string
+  ): Promise<PontosReportQueryResult> {
+    const id = parseInt(user_id || "")
+    const month = parseInt(mes || "")
+    const year = parseInt(ano || "")
+
+    if (isNaN(id) || isNaN(month) || isNaN(year)) {
+      throw new ApiRequestError(
+        400,
+        "Requisição inválida. Verifique os parâmetros fornecidos."
+      )
+    }
+
+    if (isNaN(month)) {
+      throw new ApiRequestError(
+        400,
+        "Requisição inválida. Verifique os parâmetros fornecidos."
+      )
+    }
+
+    return this.pontoRepository.workingHoursReport(
+      id,
+      month,
+      year,
+      /* c8 ignore next */
+      parseInt(limit || "20"),
+      /* c8 ignore next */
+      parseInt(offset || "0")
+    )
+  }
+}
+
+export default WorkingHoursReportUsecase
