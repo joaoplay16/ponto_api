f568a1e76cdeae19e3597ed1d06d63fec76097af
João Pedro <joaoplay16@gmail.com>
2024-02-28 22:23:25 -0300

    Cria os tipos WorkedHoursReport e PontosReportQueryResult

--- src/types/ponto.d.ts
+++ src/types/ponto.d.ts
@@ -5,7 +5,17 @@ type Ponto = {
     hora_saida: string?,
     usuario_id: number
 }
+export type WorkedHoursReport = {
+  data: string
+  dia_da_semana: string
+  horas_trabalhadas: string
+}
 
 export type PontosQueryResult = { rows: Ponto[]; count: number }
+
+export type PontosReportQueryResult = {
+  rows: WorkedHoursReport[]
+  count: number
+}
 
 export default Ponto
